333526e8b9768a9397d8f22de0a0e999121d0214
Rodrigo Bossini <professorbossini@gmail.com>
2021-11-09 20:00:33 -0300

    TrÊs displays implementados. Ainda sem polimorfismo

--- DadosClimaticos.java
+++ DadosClimaticos.java
@@ -1,3 +1,37 @@
-public class DadosClimaticos{
-    
+public class DadosClimaticos {
+    private double temperatura, umidade, pressao;
+    private CondicoesAtuaisDisplay condicoesAtuaisDisplay;
+    private EstatisticasDisplay estatisticasDisplay;
+    private PrevisoesDisplay previsoesDisplay;
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public double getUmidade() {
+        return umidade;
+    }
+
+    public double getPressao() {
+        return pressao;
+    }
+
+    public void dadosAlterados(){
+        condicoesAtuaisDisplay.atualizar(
+            getTemperatura(),
+            getUmidade(),
+            getPressao()
+        );
+
+        estatisticasDisplay.atualizar(
+            getTemperatura(),
+            getUmidade(),
+            getPressao()
+        );
+
+        previsoesDisplay.atualizar(
+            getTemperatura(),
+            getUmidade(),
+            getPressao()
+        );
+    }
 }
---
+++ EstatisticasDisplay.java
@@ -0,0 +1,5 @@
+public class EstatisticasDisplay{
+   public void atualizar (double t, double u, double p){
+
+   } 
+}
